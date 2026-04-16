6c27914f40203f445e9d3b11d47f5496e9f7d520
zhangzhiyu <xn-zhangzhiyu@cdeledu.com>
2026-04-16 17:57:02 +0800

    更新版本2.19.4

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.19.4_$(date +"%m%d")
+VERSION=2.19.5_$(date +"%m%d")
 read -p "Please enter a new version number($VERSION)：" APP_VERSION
 if [ -z "$APP_VERSION" ]; then
   APP_VERSION=$VERSION
